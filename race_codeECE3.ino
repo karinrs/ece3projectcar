d269bcf80b770790770148ab2982cf4ad2de8e4a
karinrs <karinrs2407@gmail.com>
2026-05-14 08:23:01 -0700

    Initialize sensor values and pin configurations

--- race_codeECE3.ino
+++ race_codeECE3.ino
@@ -4,13 +4,25 @@
 
 uint16_t sensorValues[8]; // right -> left, 0 -> 7
 
-const int minimums[] = 
-const int maximums[] = 
-const int weights[] = 
+const int minimums[] = {619, 596, 687, 596, 619, 711, 687, 711};
+const int maximums[] = {1881, 1904, 1812, 1904, 1881, 1789, 1813, 1789};
+const int weights[] = {-8, -4, -2, -1, 1, 2, 4, 8};
 
 const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
+const int right_nslp_pin=11;
+const int right_dir_pin=30;
+const int right_pwm_pin=39;
+
+const int photoPin1 = 65; // left side of car
+const int photoPin2 = 48;
+const int photoPin3 = 64;
+const int photoPin4 = 47;
+const int photoPin5 = 52;
+const int photoPin6 = 68;
+const int photoPin7 = 53;
+const int photoPin8 = 69; // right side of car
 
 const int LED_RF = 41;
 
